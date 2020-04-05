2ef3033acd976ebc4a0f69765dfc07bd2274d654
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-05 17:21:44 +0200

    started implementing live data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,8 +16,17 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             <Typography color='textSecondary' gutterBottom>
               Infected
             </Typography>
-            <Typography variant='h5'>{confirmed.value}</Typography>
-            <Typography color='textSecondary'>REAL DATE</Typography>
+            <Typography variant='h5'>
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=','
+              ></CountUp>
+            </Typography>
+            <Typography color='textSecondary'>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant='body2'>
               Number of active cases of COVID-19
             </Typography>
