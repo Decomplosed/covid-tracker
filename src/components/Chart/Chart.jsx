868fe3672b732833186d03c5f8f8c8230a6a3bd6
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-05 17:45:23 +0200

    imported Line and bar from chartjs

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { fetchDailyData } from '../../api'
+import { Line, Bar } from 'react-chartjs-2'
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState({})
