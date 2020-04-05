d1c0a0121fc6624cef52ec9fe75a8be7f78b2b60
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-05 17:17:01 +0200

    fixed typo in color of Typography and extracted data properly from data, also check if data is fetched, if not then show sipmly the loading information

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,16 +3,19 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 
 import styles from './Cards.module.css'
 
-const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...'
+  }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify='center'>
         <Grid item component={Card}>
           <CardContent>
-            <Typography color='textSewcondary' gutterBottom>
+            <Typography color='textSecondary' gutterBottom>
               Infected
             </Typography>
-            <Typography variant='h5'>REAL DATA</Typography>
+            <Typography variant='h5'>{confirmed.value}</Typography>
             <Typography color='textSecondary'>REAL DATE</Typography>
             <Typography variant='body2'>
               Number of active cases of COVID-19
@@ -21,7 +24,7 @@ const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
         </Grid>
         <Grid item component={Card}>
           <CardContent>
-            <Typography color='textSewcondary' gutterBottom>
+            <Typography color='textSecondary' gutterBottom>
               Recovered
             </Typography>
             <Typography variant='h5'>REAL DATA</Typography>
@@ -33,7 +36,7 @@ const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
         </Grid>
         <Grid item component={Card}>
           <CardContent>
-            <Typography color='textSewcondary' gutterBottom>
+            <Typography color='textSecondary' gutterBottom>
               Deaths
             </Typography>
             <Typography variant='h5'>REAL DATA</Typography>
