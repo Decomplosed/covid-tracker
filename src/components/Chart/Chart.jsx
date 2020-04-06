efeecf672ac4395f95904f5a13b08d4511ab4a27
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-06 12:35:29 +0200

    populated barChart with data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -38,7 +38,28 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     />
   ) : null
 
-  const barChart = data.confirmed ? <Bar /> : null
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [
+          {
+            label: 'People',
+            backgroundColor: [
+              'rgba(0, 0, 255, 0.5)',
+              'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)',
+            ],
+            data: [confirmed, recovered, deaths],
+          },
+        ],
+      }}
+      option={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null
 
   return <div className={styles.container}>{lineChart}</div>
 }
