dbc204f768ffe0dc6de0cba9abba2d67a9992322
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-06 10:15:43 +0200

    fixed styles import

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { fetchDailyData } from '../../api'
 import { Line, Bar } from 'react-chartjs-2'
 
-import './Chart.module.css'
+import styles from './Chart.module.css'
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState({})
