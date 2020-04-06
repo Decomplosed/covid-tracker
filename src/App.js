0fc330f4da49b272dbe77e567b5454215b73818d
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-06 11:00:50 +0200

    initialised handleCountryChange method and passed it to CountryPicker component as a prop

--- src/App.js
+++ src/App.js
@@ -17,12 +17,16 @@ class App extends React.Component {
     this.setState({ data: fetchedData })
   }
 
+  handleCountryChange = async (country) => {
+    
+  }
+
   render() {
     const { data } = this.state
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     )
