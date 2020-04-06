ce3512532ce918db53fe0bdd0b45a267af0c5cb2
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-06 11:00:58 +0200

    extracted out handleCountryChange

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -5,7 +5,7 @@ import styles from './CountryPicker.module.css'
 
 import { fetchCountries } from '../../api'
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
   const [fetchedCountries, setFetchedCountries] = useState([])
 
   useEffect(() => {
